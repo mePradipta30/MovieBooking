446bcce041893c25fbe55a328df1f6f2693bdaae
mePradipta30 <pradiptabanerjee02@gmail.com>
2025-11-29 20:48:48 +0530

    Update Show model schema to minimize: false

--- server/models/Show.js
+++ server/models/Show.js
@@ -6,7 +6,7 @@ const showSchema = new mongoose.Schema(
         showDateTime: { type: Date, required: true },
         showPrice: { type: Number, required: true },
         occupiedSeats: { type: Object, default: {} }
-    } , { minimize: true }
+    } , { minimize: false }
 
 )
 const Show = mongoose.model('Show', showSchema)
