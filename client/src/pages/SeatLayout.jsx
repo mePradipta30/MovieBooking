484589848a92ef328a80c36a38fe802e6581686f
mePradipta30 <pradiptabanerjee02@gmail.com>
2025-12-17 20:17:55 +0530

    `Updated SeatLayout.jsx to use showId instead of time as key and updated showDateTime display`

--- client/src/pages/SeatLayout.jsx
+++ client/src/pages/SeatLayout.jsx
@@ -115,11 +115,21 @@ const SeatLayout = () => {
         <p className='text-lg font-semibold px-6'>Available Timings</p>
         <div className='mt-5 space-y-1'>
           {show.dateTime[date].map((item) => (
-            <div key={item.time} onClick={() => setSelectedTime(item)} className={`flex items-center gap-2 px-6 py-2 w-max rounded-r-md cursor-pointer transition ${selectedTime?.time === item.time ? "bg-primary text-white" : "hover:bg-primary/20"}`}>
+            <div
+              key={item.showId}
+              onClick={() => setSelectedTime(item)}
+              className={`flex items-center gap-2 px-6 py-2 w-max rounded-r-md cursor-pointer transition ${selectedTime?.showId === item.showId
+                  ? "bg-primary text-white"
+                  : "hover:bg-primary/20"
+                }`}
+            >
               <ClockIcon className='w-4 h-4' />
-              <p className='text-sm'>{isoTimeFormat(item.time)}</p>
+              <p className='text-sm'>
+                {isoTimeFormat(item.showDateTime)}
+              </p>
             </div>
           ))}
+
         </div>
       </div>
 
